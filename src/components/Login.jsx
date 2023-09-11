7519b66696a3bb135c24c15255b5a938f35b4bea
Shohan Sharoar <maffin.killer007@gmail.com>
2023-09-11 21:52:33 +0600

    firebase login added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,29 @@
 import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/UserContext';
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext);
+    const handleSignIn = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signIn(email, password)
+            .then(result =>{
+                const user = result.user;
+                form.reset();
+                console.log(user)
+            })
+            .catch(error => console.error(error));
+    }
     return (
         <div>
             <h3 className='text-3xl font-bold mb-3'>Login</h3>
-            <form>
-                <input type="email" placeholder="Type your email here" className="input input-bordered input-success w-full max-w-xs mb-6" /> <br />
-                <input type="password" placeholder="Type your password here" className="input input-bordered input-success w-full max-w-xs" /><br />
+            <form onSubmit={ handleSignIn }>
+                <input type="email" name='email' placeholder="Type your email here" className="input input-bordered input-success w-full max-w-xs mb-6" /> <br />
+                <input type="password" name='password' placeholder="Type your password here" className="input input-bordered input-success w-full max-w-xs" /><br />
                 <button className="btn btn-outline btn-success mt-4 mr-4">Login</button>
                 <button className="btn btn-outline btn-success mt-4">Login With Google</button>
             </form>
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,4 @@
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { createContext, useState } from 'react';
 import app from '../firebase/firebase.config';
 
@@ -10,12 +10,18 @@ const UserContext = ({ children }) => {
 
     const [ user, setUser ] = useState({});
 
+    // This function stands for register user
     const createUser = ( email, password ) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    // This function stands for login user
+    const signIn = ( email, password ) =>{
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
-    const authInfo = { createUser };
+
+    const authInfo = { createUser, signIn };
     return (
         <AuthContext.Provider value={ authInfo }>
             { children }
